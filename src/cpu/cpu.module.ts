7112376e0b598a33a394ccd39690c3144ad6cbde
Muhammad Husnain <54207663+m-h9@users.noreply.github.com>
2022-03-26 00:48:00 +0500

    interact modules with each other

--- src/cpu/cpu.module.ts
+++ src/cpu/cpu.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PowerModule } from './../power/power.module';
 import { CpuService } from './cpu.service';
 
 @Module({
-  providers: [CpuService]
+  // NOW CPU MODULE HAVE ACCESS TO
+  // ALL OF SERVICE FROM POWER MODULE
+  imports: [PowerModule],
+  providers: [CpuService],
+  exports: [CpuService],
 })
 export class CpuModule {}
--- src/power/power.module.ts
+++ src/power/power.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { PowerService } from './power.service';
 
 @Module({
-  providers: [PowerService]
+  providers: [PowerService],
+  // EXPORT COPY OF POWER SERVICE TO OTHER MODULES (CPU,DISK)
+  exports: [PowerService],
 })
 export class PowerModule {}
