292aa1f2fbf9b4aa26197d63b1767f02d3cb6657
Muhammad Husnain <54207663+m-h9@users.noreply.github.com>
2022-03-29 15:51:10 +0500

    working on report

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ComputerModule } from './computer/computer.module';
+import { Report } from './report/report.entity';
+import { ReportModule } from './report/report.module';
 import { User } from './user/user.entity';
 import { UserModule } from './user/user.module';
 
@@ -9,11 +11,12 @@ import { UserModule } from './user/user.module';
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [User],
+      entities: [User, Report],
       synchronize: true,
     }),
     UserModule,
     ComputerModule,
+    ReportModule,
   ],
 })
 export class AppModule {}
